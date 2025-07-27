cf59c95717023b5901e1644e7808ae4a76d79640
zhangxuan1918 <zhangxuan1918@gmail.com>
2025-07-27 07:58:17 +0200

    fix reward function for strict format

--- code/data.py
+++ code/data.py
@@ -24,24 +24,28 @@ XML_COT_FORMAT = """<reasoning>
 {answer}
 </answer>"""
 
+
 # Loads dataset helper functions
-def get_gsm8k_dataset(split = "train"):
+def get_gsm8k_dataset(split="train"):
     data = load_dataset("openai/gsm8k", "main", split=split)
-    data = data.map(lambda x: {
-        "prompt": [
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": x["question"]},
-        ],
-        "answer": extract_hash_answer(x["answer"])
-    })
+    data = data.map(
+        lambda x: {
+            "prompt": [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": x["question"]},
+            ],
+            "answer": extract_hash_answer(x["answer"]),
+        }
+    )
     return data
 
-def get_open_math_reasoning_dataset(split = "cot"):
+
+def get_open_math_reasoning_dataset(split="cot"):
     data = load_dataset("unsloth/OpenMathReasoning-mini", split=split)
     data = data.select_columns(["problem", "generated_solution", "expected_answer"])
     # Filter out prompts with non-numeric expected answer
     data = data.filter(lambda example: example["expected_answer"].isnumeric())
-    
+
     # Format dataset
     def format_data(x):
         expected_answer = x["expected_answer"].strip()
@@ -53,13 +57,20 @@ def get_open_math_reasoning_dataset(split = "cot"):
             "prompt": [
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": problem},
-                {"role": "assistant", "content": XML_COT_FORMAT.format(reasoning=thoughts, answer=expected_answer)},
+                {
+                    "role": "assistant",
+                    "content": XML_COT_FORMAT.format(
+                        reasoning=thoughts, answer=expected_answer
+                    ),
+                },
             ],
             "answer": expected_answer,
             "thoughts": thoughts,
         }
+
     data = data.map(format_data)
     return data
+
 
 # Extract answer helper functions
 def extract_xml_answer(text: str) -> str:
@@ -70,35 +81,47 @@ def extract_xml_answer(text: str) -> str:
     else:
         return ""
 
+
 def extract_hash_answer(text: str) -> str | None:
     if "####" not in text:
         return None
     return text.split("####")[1].strip()
+
 
 # Reward functions
 def correctness_reward_func(prompts, completions, answer, **kwargs) -> list[float]:
     responses = [completion[0]["content"] for completion in completions]
     question = prompts[0][-1]["content"]
     extracted_responses = [extract_xml_answer(response) for response in responses]
-    print("-" * 20, f"Question:\n{question}", f"\nAnswer:\n{answer[0]}", f"\nResponses:\n{responses[0]}", f"\nExtracted:\n{extracted_responses[0]}")
+    print(
+        "-" * 20,
+        f"Question:\n{question}",
+        f"\nAnswer:\n{answer[0]}",
+        f"\nResponses:\n{responses[0]}",
+        f"\nExtracted:\n{extracted_responses[0]}",
+    )
     return [2.0 if r == a else 0.0 for r, a in zip(extracted_responses, answer)]
+
 
 def int_reward_func(completions, **kwargs) -> list[float]:
     responses = [completion[0]["content"] for completion in completions]
     extracted_responses = [extract_xml_answer(response) for response in responses]
     return [0.5 if r.isdigit() else 0.0 for r in extracted_responses]
 
+
 def strict_format_reward_func(completions, **kwargs) -> list[float]:
     pattern = r"^<reasoning>\n.*?\n</reasoning>\n<answer>\n.*?\n</answer>$"
     responses = [completion[0]["content"] for completion in completions]
-    matches = [re.match(pattern, r) for r in responses]
+    matches = [re.match(pattern, r, flags=re.DOTALL) for r in responses]
     return [0.75 if m else 0.0 for m in matches]
+
 
 def soft_format_reward_func(completions, **kwargs) -> list[float]:
     pattern = r"<reasoning>.*?</reasoning>\s*<answer>.*?</answer>"
     responses = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, r, flags=re.DOTALL) for r in responses]
     return [0.25 if m else 0.0 for m in matches]
+
 
 def count_xml(text: str) -> float:
     count = 0.0
@@ -112,6 +135,7 @@ def count_xml(text: str) -> float:
         count += 0.1
         count -= (len(text.split("\n</answer>\n")[-1])) * 0.001
     return count
+
 
 def xmlcount_reward_func(completions, **kwargs) -> list[float]:
     responses = [completion[0]["content"] for completion in completions]
